9f69efd5123b1295d3a64d9dce8b15f39c21a593
AngelMelendez2020 <71795575+AngelMelendez2020@users.noreply.github.com>
2020-11-29 22:18:46 -0600

    Create Nodo.java

---
+++ PilaNodo/Nodo.java
@@ -0,0 +1,12 @@
+package PilaNodo;
+
+
+public class Nodo {
+    int numero;
+    Nodo siguiente;
+    
+    public Nodo(int numero){
+        this.numero=numero;
+        this.siguiente=null;
+    }
+}
